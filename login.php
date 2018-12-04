3d501ecf1ac7379ae9c0309f1d6480550a9c144d
root <malvarezruiz@iesesteveterradas.cat>
2018-12-04 20:17:33 +0100

    g

--- login.php
+++ login.php
@@ -1,58 +1,92 @@
-<?php 
-	session_start();
-?>
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
 	<title>LOGIN</title>
-	<script type="text/javascript" src="loginFunciones.js"></script>
+	
+	<script type="text/javascript" src="loginFunciones.js" ></script>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<?php
-	if (isset($_SESSION["usuario"]) && isset($_SESSION["pass"])) {
-		header("projectos.php");
-	} else {
-
-		$_POST["usuario"] = "";
-		$_POST["password"] = "";
-
-		echo "<div id='header'>";
-			echo "¡ Bienvenido !";
-		echo "</div>";
+	<div id='header'>
 		
-		echo "<div id='center' >";
-			echo "<form action='login.php' method='POST'>";
-				echo "<label> Usuario: </label>";
-				echo "<input type='text' name='usuario'>";
-				echo "<br>";
-				echo "<label> Contraseña: </label>";
-				echo "<input type='password' name='password'>";
-				echo "<br>";
-				echo "<input type='submit' name='enviar' value='Aceptar'>";
-				echo "<a href='#'> ¿ Olvidaste la contraseña ? </a>";
+	</div>
+	<div id='center' >
+		<section class="section section-login">
+	            <div class="valign-wrapper row login-box">
+	                    <div class="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
+	                      <form action="#" method="POST">
+	                        <div class="card-content">
+	                          <span class="card-title">Login</span>
+	                          <div class="row">
+	                          	
+	                            <div class="input-field col s12">
+	                              <label for="text">usuario </label>
+	                              <br>
+	                              <input type="text" class="validate" name="usuario"  />
+	                            </div>
+	                            <div class="input-field col s12">
+	                              <label for="password">contraseña </label>
+	                              <br>
+	                              <input type="password" class="validate" name="passwd"  />
+	                            </div>
+	                          </div>
+	                        </div>
+	                        <div class="card-action right-align">
+	                            <input type="submit" class="btn teal waves-effect waves-light" value="Login">
+	                        </div>
+	                      </form>
+	                    </div>
+	            </div>
+	</section>
+	</div>
 
-				$usuario = $_POST["usuario"];
-				$password = $_POST["password"];
-
-			echo "</form>";			
-
-		echo "</div>";
-		
-		echo "<div id='mensajeError'>";
-
-		echo "</div>";
-		
-		echo "<div id='footer'>";
-
-		echo "</div>";
-
-		echo "<button onclick='error()' > HOLA </button>";
-
-		
-	}
+	<div id='mensajeError'>
 	
+	</div>
+	
+	<div id='footer'>
+		<button onclick='mensajeError("holaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholahola")' > HOLA </button>
+	</div>
 
-	?>
+	<?php
 
+		$user=$_POST['usuario'];
+		$pass=$_POST['passwd'];
+		$resultUser=2;
+		$resultPass=2;
+		$dbs= "mysql:host=localhost;dbname=GestorProjectes";
+		$dbh = new PDO( $dbs, "marc","marc123");
+	 
+		$consultaUsuario = $dbh->prepare("SELECT * FROM usuarios WHERE usuario=:user");
+		$consultaPassword = $dbh->prepare("SELECT * FROM usuarios WHERE password=SHA2(:pass,512) ");
+	    $consultaUsuario->bindValue(':user', $user);
+		$consultaPassword->bindValue(':pass', $pass);
+		$consultaUsuario->execute();
+		$consultaPassword->execute();
+	 	
+		$resultUser=$consultaUsuario->rowCount();
+		$resultPass=$consultaPassword->rowCount();
+		
+		/*if($resultUser==1 and $resultPass==1){
+			echo"loginCorrecto()";
+		}
+		elseif($resultUser==1 and $resultPass!=1){
+			echo"errorPassword()";
+		}
+		elseif ($resultUser!=1 and $resultPass==1) {
+			echo"errorUser()";
+		}
+		elseif($resultUser!=1 and $resultPass!=1){
+			echo"errorLogin()";
+		}
+		*/
+
+
+	?>	
 </body>
 </html>
--- loginFunciones.js
+++ loginFunciones.js
@@ -1,14 +1,34 @@
-function error() {
+function mensajeError(texto) {
+	//Guardo en una variable del div especial para los errores
 	var zonaError = document.getElementById("mensajeError");
 
-	//Creo una variable donde guardar el mensaje de error
-	var contenido = document.createTextNode("ERROR");
+	
+	//Creo un div para meter el icono y el texto dentro
+	var divError = document.createElement("div");
+	//Anado id al div que tiene el error 
+	divError.setAttribute("id","contenedorError")
+	//Anado el divError como hijo del zonaError
+	zonaError.appendChild(divError)
 
 	//Creo una variable iconoError que sera img
 	var iconoError = document.createElement("img");
+	//Indico cual sera el src de la imagen
 	iconoError.src = "assets/error.png";
+	//Anado el atributo de id y le digo que sea errorImg
+	iconoError.setAttribute("id","errorImg");
+	//Anado la variable que he creado como hijo
+	divError.appendChild(iconoError);
 
-	//Anado la variable que he creado como hijo del child
-	zonaError.appendChild(iconoError);
+	//Creo una p para guardar el texto
+	var pTextoError = document.createElement("p");
+	//Creo una variable donde meto el texto
+	var texto = document.createTextNode(texto);
+	//Anado a la p el texto
+	pTextoError.appendChild(texto)
+
+	//Le anado al divError la p
+	divError.appendChild(pTextoError);
+
+	divError.setAttribute("class","parpadea");
 	
 }
